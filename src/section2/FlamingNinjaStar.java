a254e26563300d833cd551b44f38bfae95c06304
Cyrooster <league@WTS-iMac-11.attlocal.net>
2018-02-23 14:46:20 -0800

    Day 4

--- src/section2/FlamingNinjaStar.java
+++ src/section2/FlamingNinjaStar.java
@@ -14,13 +14,14 @@ Robot trumpsfrend = new Robot();
 trumpsfrend.penDown();
 trumpsfrend.setSpeed(10);
 		// *14. Use the methods setX and setY to move the ninja star into the center of the screen
-		
+		trumpsfrend.setX(450);
+		trumpsfrend.setY(270);
 		// *15. Make some adjustments to see what other kinds of shapes you can make.
 
 		// 12. Set the robot speed to 10
 
 		// 13. Use a for loop to repeat all of the code below 25 times
-
+for(int i=0;i<25;i++) {
 			// 2. Turn the robot 1/8 of a circle
 trumpsfrend.turn(45);
 		
@@ -38,12 +39,13 @@ trumpsfrend.move(64);
 			// 8. Turn the robot 64 degrees to the right
 			trumpsfrend.turn(64);
 			// 9. Move the robot the distance in the variable baseSize
-			
+			trumpsfrend.setPenColor(0, 0, 0);
 			trumpsfrend.move(baseSize);
 			// 10. Check that your shape is the same as Figure 1. This is one arm of the ninja star.
 			
 			// 11. Color your ninja star like Figure 2.
 		
+	}
 	}
 
 }
